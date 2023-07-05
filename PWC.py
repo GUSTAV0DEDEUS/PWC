be8e692dee3a2b7f889d82758814cba407eb70e9
GUSTAV0DEDEUS <gustavodedeusconceicao@gmail.com>
2023-07-05 09:41:03 -0300

    Segundo Desafio :tada: Depois de utilizar o for in,  ficou simples validar as letras duplicadas. (Cases Comentados)

--- PWC.py
+++ PWC.py
@@ -8,3 +8,17 @@ print(newPhrase)
 
 # CASO1: Hello, world! openIA is amazing
 # CASO2: Good Morning, How are you?
+
+#2
+phrase = input().split()
+newPhrase = ""
+for i in range(len(phrase)):
+  noDuplicates = ""
+  for letter in phrase[i]:
+    if letter not in noDuplicates:
+     noDuplicates += letter
+  newPhrase += noDuplicates + " "
+print(newPhrase)
+
+#CASO1: Hello, world!
+#CASO2: Good Morning
