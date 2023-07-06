ae38447164beb26fce716b8d9a255408ea9cc336
Gustavo de Deus <88512041+GUSTAV0DEDEUS@users.noreply.github.com>
2023-07-06 19:21:18 -0300

    Desafios finalizados :tada:
    Percebi que o desafio 2 estava um pouco distante do esperado.

--- PWC.py
+++ PWC.py
@@ -1,36 +1,35 @@
 # 1
-# phrase = input().split()
-# newPhrase = ''
-# for i in range(len(phrase)):
-#   newPhrase += str(phrase[-1]) + " "
-#   phrase.pop(-1)
-# print(newPhrase)
+phrase = input().split()
+newPhrase = ''
+for i in range(len(phrase)):
+  newPhrase += str(phrase[-1]) + " "
+  phrase.pop(-1)
+print(newPhrase)
 
-# # CASO1: Hello, world! openIA is amazing
-# # CASO2: Good Morning, How are you?
+# CASO1: Hello, world! openIA is amazing.
+# CASO2: Good Morning, How are you?
 
-# #2
-# phrase = input().split()
-# newPhrase = ""
-# for i in range(len(phrase)):
-#   noDuplicates = ""
-#   for letter in phrase[i]:
-#     if letter not in noDuplicates:
-#      noDuplicates += letter
-#   newPhrase += noDuplicates + " "
-# print(newPhrase)
+#2
+phrase = list(input())
+newPhrase = ""
+noDuplicates = ""
+for letter in phrase:
+   if letter not in noDuplicates:
+      noDuplicates += letter   
+newPhrase = noDuplicates
+print(newPhrase)
 
 #CASO1: Hello, world!
 #CASO2: Good Morning
 
 #3
-# word = input()
-# while True:
-#   if(len(word) == 0 or word[::-1] in word) :
-#     print(word)
-#     break
-#   else:
-#     word = word.rstrip(word[-1])   
+word = input()
+while True:
+  if(len(word) == 0 or word[::-1] in word) :
+    print(word)
+    break
+  else:
+    word = word.rstrip(word[-1])   
     
 #CASO1: BABAD
 #CASO2: OVOWOD
@@ -50,6 +49,17 @@ for i in range(len(phrase) - 1):
     else:
       newPhrase +=  " " + phrase[i + 1]
 print(newPhrase)
-
 #CASO1: hello. how are you? i'm fine, thank you.
 #CASO2: wow. you are doctor, too? yes, i started my job this week! great, congratulations.
+
+#5
+word = input()
+newWord = ''
+for i in range(len(word)):
+  newWord += word[-(i+1)]
+if newWord == word:
+  print('true')
+else:
+  print('false')
+#CASO1: socorrammesubinoonibusemmarrocos
+#CASO2: racecar
