daea5b3fa44c97b0e0c5b3d6d3a0b8da98f82604
Gustavo de Deus <88512041+GUSTAV0DEDEUS@users.noreply.github.com>
2023-07-06 20:29:53 -0300

    Revisão :tada:
    Revisão e alterações para aperfeiçoamento dos desafios.

--- PWC.py
+++ PWC.py
@@ -1,4 +1,4 @@
-# 1
+# 1 - Reverta a ordem das palavras nas frases, mantendo a ordem das palavras.
 phrase = input().split()
 newPhrase = ''
 for i in range(len(phrase)):
@@ -9,20 +9,23 @@ print(newPhrase)
 # CASO1: Hello, world! openIA is amazing.
 # CASO2: Good Morning, How are you?
 
-#2
-phrase = list(input())
+#2 Remova todos os caracteres duplicados da string abaixo
+phrase = input().split()
 newPhrase = ""
 noDuplicates = ""
-for letter in phrase:
-   if letter not in noDuplicates:
-      noDuplicates += letter   
-newPhrase = noDuplicates
+for i in range(len(phrase)):
+  temp = ''
+  for letter in phrase[i]:
+    if letter not in noDuplicates:
+        noDuplicates += letter 
+        temp += letter  
+  newPhrase += temp + " "
 print(newPhrase)
 
 #CASO1: Hello, world!
 #CASO2: Good Morning
 
-#3
+#3 Enconte a substring palíndroma mais longa na string abaixo
 word = input()
 while True:
   if(len(word) == 0 or word[::-1] in word) :
@@ -34,7 +37,7 @@ while True:
 #CASO1: BABAD
 #CASO2: OVOWOD
 
-#4
+#4 Coloque em maiúscula a primeira letra de cada frase da string
 phrase = input().split()
 substrings = ['.', ";", "!", "?"]
 newPhrase = phrase[0].capitalize()
@@ -49,17 +52,19 @@ for i in range(len(phrase) - 1):
     else:
       newPhrase +=  " " + phrase[i + 1]
 print(newPhrase)
+
 #CASO1: hello. how are you? i'm fine, thank you.
 #CASO2: wow. you are doctor, too? yes, i started my job this week! great, congratulations.
 
-#5
+#5 Verifique se a string é um anagrama de um palíndromo
 word = input()
 newWord = ''
 for i in range(len(word)):
   newWord += word[-(i+1)]
 if newWord == word:
-  print('true')
+  print("true")
 else:
-  print('false')
+  print("false")
+  
 #CASO1: socorrammesubinoonibusemmarrocos
 #CASO2: racecar
